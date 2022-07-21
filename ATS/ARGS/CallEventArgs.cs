6cb2dcec2933fcfb795841c731362ba3f1be5af0
Dima <DimaBrisset@gmail.com>
2022-07-21 13:01:30 +0300

    ert

---
+++ ATS/ARGS/CallEventArgs.cs
@@ -0,0 +1,26 @@
+﻿namespace ATS
+{
+    public class CallEventArgs : EventArgs
+    {
+        public string CallerPhoneNumber { get; set; }
+
+        public string TargetPhoneNumber { get; }
+
+        public CallResponseCode ResponseCode { get; set; }
+
+        public CallErrorCode ErrorCode { get; set; }
+
+        public DateTime DateTimeBeginCall { get; set; }
+
+        public DateTime DataTimeEndCall { get; set; }
+
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        public CallEventArgs(string targetPhoneNumber)
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        {
+            TargetPhoneNumber = targetPhoneNumber;
+            ErrorCode = CallErrorCode.UNKNOWN;
+            ResponseCode = CallResponseCode.UNKNOWN;
+        }
+    }
+}
